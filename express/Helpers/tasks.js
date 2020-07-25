f97d53de2b773dd5daae273f55b47d0e6e3c86b5
shrutikumar15 <41858958+shrutikumar15@users.noreply.github.com>
2020-07-25 17:08:57 +0530

    Get route for admin to see the users

--- express/Helpers/tasks.js
+++ express/Helpers/tasks.js
@@ -15,6 +15,25 @@ app.post("/dailyUpdate",(req,res)=>{
     })
 })
 
+app.get('/admin/assign/', (req, res)=>{
+    var users = [];
+    const ref =admin.firestore();
+    ref.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            let data = doc.data();
+            var user={};
+            user.id=doc.id;
+            user.name = data.name;
+            user.availability = data.timeAvail
+            users.push(user);
+        })
+        console.log(users);  
+        return res.status(200).json(users);
+    })
+
+ });
+
+
 
 
 module.exports = app
